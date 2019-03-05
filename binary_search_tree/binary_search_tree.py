e281284b80800c65e32e242dd03e65c4bbc742b5
zacharyarney <zachary.arney@gmail.com>
2019-03-05 13:22:46 -0800

    BST contains method rough draft

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,21 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        pass
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        while self is not None:
+            if target < self.value:
+                self = self.left
+            if target > self.value:
+                self = self.right
+            if target == self.value:
+                return True
+        return False
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
