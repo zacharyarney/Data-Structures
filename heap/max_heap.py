7fa1dc72a4d6c5a539bbc7defdecec97cb197ee0
zacharyarney <zachary.arney@gmail.com>
2019-03-06 15:43:44 -0800

    bubble up and sift down methods implemented but not tested. need insert and delete to test.

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,43 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        pass
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        store = self.storage
+        parent = (index - 1) // 2
+        if parent:
+            if store[index] > store[parent]:
+                store[index], store[parent] = store[parent], store[index]
+                return _bubble_up(parent)
+            if store[index] < store[parent]:
+                return
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        store = self.storage
+        lchild = (index * 2) + 1
+        rchild = (index * 2) + 2
+        if store[lchild] and store[rchild]:
+            if store[rchild] > store[lchild]:
+                if store[rchild] > store[index]:
+                    store[index], store[rchild] = store[rchild], store[index]
+                    return _sift_down(rchild)
+                else:
+                    return
+            if store[lchild] > store[rchild]:
+                if store[lchild] > store[index]:
+                    store[index], store[lchild] = store[lchild], store[index]
+                    return _sift_down(lchild)
+                else:
+                    return
