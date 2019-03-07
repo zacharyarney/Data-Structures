a6cc883651f16404b0a008113e4ad3f765b94b6e
zacharyarney <zachary.arney@gmail.com>
2019-03-06 17:52:48 -0800

    insert and bubble up working. get_size typo fixed.

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,41 +3,47 @@ class Heap:
         self.storage = []
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        pass
+        # deleted = self.get_max()
+        store = self.storage
+        store[0], store[len(store) - 1] = store[len(store) - 1], store[0]
+        self._sift_down()
+        store.pop()
 
     def get_max(self):
         return self.storage[0]
 
     def get_size(self):
-        return len(self.storag)
+        return len(self.storage)
 
     def _bubble_up(self, index):
         store = self.storage
         parent = (index - 1) // 2
-        if parent:
+        if parent >= 0:
             if store[index] > store[parent]:
                 store[index], store[parent] = store[parent], store[index]
                 return self._bubble_up(parent)
-            if store[index] < store[parent]:
+            else:
                 return
 
     def _sift_down(self, index):
         store = self.storage
-        lchild = (index * 2) + 1
-        rchild = (index * 2) + 2
-        if store[lchild] and store[rchild]:
-            if store[rchild] > store[lchild]:
-                if store[rchild] > store[index]:
-                    store[index], store[rchild] = store[rchild], store[index]
-                    return self._sift_down(rchild)
-                else:
-                    return
-            if store[lchild] > store[rchild]:
-                if store[lchild] > store[index]:
-                    store[index], store[lchild] = store[lchild], store[index]
-                    return self._sift_down(lchild)
-                else:
-                    return
+        lc = (index * 2) + 1
+        rc = (index * 2) + 2
+        if lc < len(store):
+            if rc < len(store):
+                if store[rc] > store[lc]:
+                    if store[rc] > store[index]:
+                        store[index], store[rc] = store[rc], store[index]
+                        return self._sift_down(rc)
+                    else:
+                        return
+                if store[lc] > store[rc]:
+                    if store[lc] > store[index]:
+                        store[index], store[lc] = store[lc], store[index]
+                        return self._sift_down(lc)
+                    else:
+                        return
